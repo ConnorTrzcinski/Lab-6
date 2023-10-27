5accac030188884e01250bfcd43ee86f05ef983f
ConnorTrzcinski <connor.trzcinski@ufl.edu>
2023-10-27 16:06:16 -0400

    Updated Decoder Function

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #Decoder code
 
-def decode(user_password):
-    decoded_password = ""
-    for digit in user_password:
-        the_password = str((int(digit)-3)%10)
-        decoded_password += user_password
-    return decoded_password
+def decoder(password):
+    pass_str = str(password)
+    pass_list = list(pass_str)
+    value= []
+    for c in pass_list:
+        value.append(int(c) // 3)
+        final = ''.join(map(str, value))
+    return final
 
